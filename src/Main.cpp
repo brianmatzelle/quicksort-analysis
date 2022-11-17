7df7695a25612a9c697d86c98f876adfa58164b5
Brian Matzelle <brianmatzelle@Brians-MacBook-Air-2.local>
2022-11-17 18:48:09 -0500

    added comments to main.cpp

--- src/Main.cpp
+++ src/Main.cpp
@@ -4,6 +4,10 @@
 #include "Median.h"
 
 int main() {
-
+    /*
+    Where we'll run quicksort?
+    Each cpp file will be a partition...
+    maybe we create a generic quicksort() function then just insert the different partitions?
+    */
     return 0;
 }
