ebb8f39d870a17b0f38b9c857e22f05ed158af8c
Brian Matzelle <brianmatzelle@Brians-MacBook-Air-2.local>
2022-11-30 03:13:54 -0500

    removed some comments

--- generate_samples/src/backwards.rs
+++ generate_samples/src/backwards.rs
@@ -1,12 +1,3 @@
-// pub fn backwards(size: u16) -> String {
-//     let mut ret = String::new();
-//     for i in (0..size).rev() {
-//         ret.push_str(&i.to_string());
-//         ret.push_str("\n");
-//     }
-//     ret
-// }
-
 fn generate_one_set(size: u16) -> String {
     let mut ret = String::new();
     ret.push_str(&size.to_string());
@@ -21,8 +12,6 @@ fn generate_one_set(size: u16) -> String {
 pub fn backwards(size: u16) -> String {
     let mut ret = String::new();
     for set_size in (1..size) {
-        // let mut generated_set: String = String::new();
-        // generated_set.push_str(&generate_one_set(set_size));
         ret.push_str(&generate_one_set(set_size));
         ret.push_str("\n");
     }
--- generate_samples/src/in_order.rs
+++ generate_samples/src/in_order.rs
@@ -1,12 +1,3 @@
-// pub fn in_order(size: u16) -> String {
-//     let mut ret = String::new();
-//     for i in 0..size {
-//         ret.push_str(&i.to_string());
-//         ret.push_str("\n");
-//     }
-//     ret
-// }
-
 fn generate_one_set(size: u16) -> String {
     let mut ret = String::new();
     ret.push_str(&size.to_string());
