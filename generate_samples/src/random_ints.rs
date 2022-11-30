0628acd4a9091dab354eb5185c41090d746162f9
Brian Matzelle <brianmatzelle@Brians-MacBook-Air-2.local>
2022-11-30 03:16:01 -0500

    removed some comments

--- generate_samples/src/random_ints.rs
+++ generate_samples/src/random_ints.rs
@@ -15,8 +15,6 @@ fn generate_one_set(size: u16, lower_b: i32, upper_b: i32) -> String {
 pub fn random_ints(size: u16, lower_b: i32, upper_b: i32) -> String {
     let mut ret = String::new();
     for set_size in 1..size {
-        // let mut generated_set: String = String::new();
-        // generated_set.push_str(&generate_one_set(set_size, lower_b, upper_b));
         ret.push_str(&generate_one_set(set_size, lower_b, upper_b));
         ret.push_str("\n");
     }
